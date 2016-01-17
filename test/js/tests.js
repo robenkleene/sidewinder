0d476e212dc7579ae5b1e205443871b40947823f
Roben Kleene <robenk@gmail.com>
2016-01-16 22:25:20 -0500

    Revise test cases

--- test/js/tests.js
+++ test/js/tests.js
@@ -1,20 +1,20 @@
 describe('euclidean', function() {
-  it('should return the right value for 5 pulses and 8 steps', function () {
-		euclidean(5, 8).should.deep.equal([1,0,1,1,0,1,1,0]);
+  it('should return the right value for 1 pulses and 2 steps', function () {
+		euclidean(1,2).should.deep.equal([1,0]);
   });
-	it('should return the right value for 3 pulses and 8 steps', function () {
-		euclidean(3, 8).should.deep.equal([1,0,0,1,0,0,1,0]);
-	});
+  it('should return the right value for 1 pulses and 3 steps', function () {
+		euclidean(1,3).should.deep.equal([1,0,0]);
+  });
+  it('should return the right value for 1 pulses and 4 steps', function () {
+		euclidean(1,4).should.deep.equal([1,0,0,0]);
+  });
+  it('should return the right value for 4 pulses and 12 steps', function () {
+		euclidean(4,12).should.deep.equal([1,0,0,1,0,0,1,0,0,1,0,0]);
+  });
 	it('should return the right value for 2 pulses and 3 steps', function () {
     euclidean(2,3).should.deep.equal([1,0,1]);
 	});
 	it('should return the right value for 2 pulses and 5 steps', function () {
-    euclidean(2,5).should.deep.equal([1,0,1,0,0]);
-	});
-	it('should return the right values for 2 pulses and 3 steps', function () {
-    euclidean(2,3).should.deep.equal([1,0,1]);
-	});
-	it('should return the right values for 2 pulses and 5 steps', function () {
     euclidean(2,5).should.deep.equal([1,0,1,0,0]);
 	});
 	it('should return the right values for 3 pulses and 4 steps', function () {
