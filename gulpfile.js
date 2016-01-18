83c5cc355f957c92e05e492a74f528666b043cc8
Roben Kleene <robenk@gmail.com>
2016-01-18 18:02:01 -0500

    Add WIP gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+var path = require('path');
+
+var paths = {
+    src: './src/',
+    build: './max/'
+};
+
+gulp.task('coffee', function() {
+  gulp.src(path.join(paths.src, 'cofee'))
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest(path.join(paths.src, 'js')));
+});
+
+gulp.task('watch', function() {
+    gulp.watch([paths.src + 'coffee/*.litcoffee'], ['coffee']);
+});
+
+gulp.task('default', ['coffee']);
