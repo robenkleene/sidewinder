a1125dc2db5c746310f6de3108e2e8ce7edf4b5a
Roben Kleene <robenk@gmail.com>
2016-01-18 13:59:44 -0500

    Make work with one example of the subtraction step

--- max/js/euclidean2.js
+++ max/js/euclidean2.js
@@ -7,18 +7,18 @@ function bjorklund(pulses, steps) {
   var groupA = [];
   var groupB = [];
 
-  var flattenArray = function(value) {
-    var result = [];
-    for (var i = 0; i < value.length; i++) {
-      if(Array.isArray(value[i])) {
-        var flattened = flattenArray(value[i])
-        result = result.concat(flattened);
-      } else {
-        result.push(value[i]);
-      }
-    }
-    return result;
-  }
+  // var flattenArray = function(value) {
+  //   var result = [];
+  //   for (var i = 0; i < value.length; i++) {
+  //     if(Array.isArray(value[i])) {
+  //       var flattened = flattenArray(value[i])
+  //       result = result.concat(flattened);
+  //     } else {
+  //       result.push(value[i]);
+  //     }
+  //   }
+  //   return result;
+  // }
 
   for (var i = 0; i < steps; i++) {
     if (i < pulses) {
@@ -30,17 +30,32 @@ function bjorklund(pulses, steps) {
 
   var count = Math.floor(groupB.length / groupA.length);
   for (i = 0; i < groupA.length; i++) {
-    var removed = groupB.splice(0, count);
-    removed = flattenArray(removed);
-    groupA[i] = groupA[i].concat(removed);
+    var removed = groupB.splice(groupB.length - count, count);
+    for (var j = 0; j < count; j++) {
+      groupA[i] = groupA[i].concat(removed[j]);
+    }
   }
 
+  console.log("groupA = " + JSON.stringify(groupA));
+  console.log("groupB = " + JSON.stringify(groupB));
 
-  var bitsToRemove = Math.floor(groupA.length / groupB.length);
-  
 
-console.log("groupA = " + JSON.stringify(groupA));
-console.log("groupB = " + JSON.stringify(groupB));
+  var stringsToRemove = Math.floor(groupA.length / groupB.length);
+  var lengthOfStringToRemove = groupB.length;
+
+  var joined = groupA.concat(groupB);
+
+console.log("joined = " + JSON.stringify(joined));
+
+  // Merge this with the above similar method
+  for (i = 0; i < stringsToRemove; i++) {
+    var removed = joined.splice(joined.length - lengthOfStringToRemove, lengthOfStringToRemove);
+    for (j = 0; j < removed.length; j++) {
+      joined[j] = joined[j].concat(removed[j]);
+    }
+  }
+
+console.log("joined = " + JSON.stringify(joined));
 }
 
 bjorklund(7, 17);
