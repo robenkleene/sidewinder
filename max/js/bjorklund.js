5d426836fc14cd3915bc561c9b765077237b8a94
Roben Kleene <robenk@gmail.com>
2016-01-18 17:16:04 -0500

    Remove flatten array dependency

--- max/js/bjorklund.js
+++ max/js/bjorklund.js
@@ -4,26 +4,13 @@
 // steps: n
 
 function bjorklund(pulses, steps) {
-console.log("pulses = " + pulses);
-console.log("steps = " + steps);
+// console.log("pulses = " + pulses);
+// console.log("steps = " + steps);
 
   var groupA = [];
   var groupB = [];
 
-  var flattenArray = function(value) {
-    var result = [];
-    for (var i = 0; i < value.length; i++) {
-      if(Array.isArray(value[i])) {
-        var flattened = flattenArray(value[i])
-        result = result.concat(flattened);
-      } else {
-        result.push(value[i]);
-      }
-    }
-    return result;
-  }
-
-console.log("Initialization");
+// console.log("Initialization");
 
   for (var i = 0; i < steps; i++) {
     if (i < pulses) {
@@ -33,8 +20,8 @@ console.log("Initialization");
     }
   }
   
-console.log("groupA = " + JSON.stringify(groupA));
-console.log("groupB = " + JSON.stringify(groupB));
+// console.log("groupA = " + JSON.stringify(groupA));
+// console.log("groupB = " + JSON.stringify(groupB));
   
   // var count = Math.floor(groupB.length / groupA.length);
   // for (i = 0; i < groupA.length; i++) {
@@ -44,24 +31,24 @@ console.log("groupB = " + JSON.stringify(groupB));
   //   }
   // }
 
-console.log("groupA = " + JSON.stringify(groupA));
-console.log("groupB = " + JSON.stringify(groupB));
-
-console.log("Subtraction");
+// console.log("groupA = " + JSON.stringify(groupA));
+// console.log("groupB = " + JSON.stringify(groupB));
+// 
+// console.log("Subtraction");
 
   if (groupB.length > 0) {
     do {
     // while (groupB.length > 1) {
       var stringsToRemove = Math.floor(groupA.length / groupB.length);
       var lengthOfStringToRemove = groupB.length;
-console.log("groupA.length = " + groupA.length);
-console.log("groupB.length = " + groupB.length);
-console.log("stringsToRemove = " + stringsToRemove);
-console.log("lengthOfStringToRemove = " + lengthOfStringToRemove);
+// console.log("groupA.length = " + groupA.length);
+// console.log("groupB.length = " + groupB.length);
+// console.log("stringsToRemove = " + stringsToRemove);
+// console.log("lengthOfStringToRemove = " + lengthOfStringToRemove);
 
       var joined = groupA.concat(groupB);
 
-console.log("joined = " + JSON.stringify(joined));
+// console.log("joined = " + JSON.stringify(joined));
 
       // Merge this with the above similar method
       for (i = 0; i < stringsToRemove; i++) {
@@ -71,17 +58,17 @@ console.log("joined = " + JSON.stringify(joined));
         }
       }
 
-console.log("joined = " + JSON.stringify(joined));
+// console.log("joined = " + JSON.stringify(joined));
 
       var splicePoint = groupB.length;
       groupA = joined.splice(0, splicePoint);
       groupB = joined;
 
-console.log("groupA = " + JSON.stringify(groupA));
-console.log("groupB = " + JSON.stringify(groupB));
+// console.log("groupA = " + JSON.stringify(groupA));
+// console.log("groupB = " + JSON.stringify(groupB));
     } while (groupB.length > 1);
   }
 
-
-  return flattenArray(groupA.concat(groupB));
+  var final = groupA.concat(groupB);
+  return [].concat.apply([], final);
 }
