6a5eee88329ccff960e23566fdf86e1dc7d19199
Roben Kleene <contact@robenkleene.com>
2024-02-26 05:47:44 -0500

    Fix formatting

--- javascript/sidewinder_randomize.js
+++ javascript/sidewinder_randomize.js
@@ -24,9 +24,9 @@ function getRandomArbitrary(min, max) {
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function msg_float(value) {
