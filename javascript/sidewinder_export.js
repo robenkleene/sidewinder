b02d151aa0e4f766fc4c917d82688b1f77f7990a
Roben Kleene <contact@robenkleene.com>
2025-01-20 17:59:13 -0500

    Get values

--- javascript/sidewinder_export.js
+++ javascript/sidewinder_export.js
@@ -4,6 +4,12 @@ outlets = 1;
 
 function bang() {
   var d = new Dict("export");
-  var keys = d.getkeys();
-  outlet(0, keys);
+  var pitches = d.get("pitch");
+  var velocities = d.get("velocity");
+  var durations = d.get("duration");
+  var interval = d.get("interval");
+  outlet(0, pitches);
+  outlet(0, velocities);
+  outlet(0, durations);
+  outlet(0, interval);
 }
