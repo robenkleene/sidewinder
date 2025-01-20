cc56fa21bdde9d1816b9d8a7a9b8519b74702703
Roben Kleene <contact@robenkleene.com>
2025-01-20 18:07:55 -0500

    Setup make notes

--- javascript/sidewinder_export.js
+++ javascript/sidewinder_export.js
@@ -1,6 +1,5 @@
 // Inlets & Outlets
 inlets = 1;
-outlets = 1;
 
 function bang() {
   var d = new Dict("export");
@@ -8,8 +7,23 @@ function bang() {
   var velocities = d.get("velocity");
   var durations = d.get("duration");
   var interval = d.get("interval");
-  outlet(0, pitches);
-  outlet(0, velocities);
-  outlet(0, durations);
-  outlet(0, interval);
+  d.clear();
+  var notes = makeNotes(pitches, durations, velocities, durations, interval);
+}
+
+function makeNotes(pitches, durations, velocities, durations, interval) {
+  var startTime = 0;
+  var notes = [];
+  for (var i = 0; i < pitches.length; i++) {
+    var note = {};
+    if (velocities[i] > 0) {
+      note["start_time"] = startTime;
+      note["pitch"] = pitches[i];
+      note["velocity"] = velocities[i];
+      note["duration"] = durations[i];
+      notes.push(note);
+    }
+    startTime += interval;
+  }
+  return notes;
 }
